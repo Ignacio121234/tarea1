a8eae7aea82770c80474dd5bb982ecdd82d54003
ignacio121234 <igarcia2020@udec.cl>
2025-05-02 22:35:48 -0400

    se agregan algunos print statements a main

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,28 @@ public class Main {
     public static void main(String[] args) {
         Expendedor b =new Expendedor(5);
         Comprador p = new Comprador(new Moneda100(),1,b);
-        Comprador x = new Comprador(new Moneda1000(),1,b);
+        Comprador x = new Comprador(new Moneda100(),1,b);
         Comprador y = new Comprador(new Moneda1000(),2,b);
         Comprador z = new Comprador(new Moneda1000(),3,b);
-        Comprador a = new Comprador(new Moneda1000(),4,b);
-        Comprador d = new Comprador(new Moneda1000(),4,b);
+        Comprador a = new Comprador(new Moneda500(),4,b);
+        Comprador d = new Comprador(new Moneda500(),4,b);
         Comprador e = new Comprador(new Moneda1000(),4,b);
         Comprador f = new Comprador(new Moneda1000(),4,b);
         Comprador g = new Comprador(new Moneda1000(),4,b);
         Comprador h = new Comprador(new Moneda1000(),4,b);
-        System.out.println(x.cuantoVuelto());
+        System.out.println("vuelto:"+e.cuantoVuelto());
+        System.out.println("vuelto:"+f.cuantoVuelto());
+        System.out.println("vuelto:"+y.cuantoVuelto());
+        System.out.println("vuelto:"+g.cuantoVuelto());
+        System.out.println("vuelto:"+h.cuantoVuelto());
+
+        System.out.println(p.QueProducto());
+        System.out.println(d.QueProducto());
+
+
+
+
+
 
 
 
